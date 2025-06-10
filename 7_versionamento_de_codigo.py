99bee80060ac0978932953f5ce9131ceed17234f
Raylan Araujo <raylanra@gmail.com>
2025-06-10 17:53:13 -0300

    anotacoes da aula de versionamento de versões

---
+++ 7_versionamento_de_codigo.py
@@ -0,0 +1,25 @@
+# Versionamento de código
+#   cada alteraçao que voce faz no seu código cria uma nova versao.
+#   
+#   Surgiram de controle de versao
+#       sao softwares que controlam as versoes de um arquivo ao longo do tempo registrando um historico detalhado de cada alteração feita.
+#       eles gerenciam quais alterações foram feitas, a data, o autor
+#       organizacao, controle e seguranca
+
+#       Tipos de sistema de controle de versao
+#           VSC Centralizados (CVCS)
+#           Exemplo: CVS, Subversion
+#           Nele tem apenas um servidor onde vai conter todos os arquivos responsaveis pelo controle de versao. 
+#           Nesse caso, voce e seu amigo estarão conectados ao servidos, mas se ele ficar fora do ar voce acaba nao conseguindo salvar  ou colaborar.
+#           Se der problema e nao tiver sido feito um backup direito, todos os dados são perdidos.
+
+
+
+
+#           VCS Distribuído (DVCS)
+#           Exemplo: Git e Mercurial
+#           
+#           Surgiram para resolver as desvantagens dos sistemas centralizados
+#           Nele, cada banco de versao é duplicado localmente. Assim voce e seu amigo tera uma copia do que esta no servidor central e permite que seja trabalhado mesmo que o servidor esteja fora do ar.
+#
+#           Clona o repositorio completo o que inclui o histórico de versões. Cada clone serve de backup. Possibilita um trabalho flexível.
