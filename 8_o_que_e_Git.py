846141e7b565b5595d1a58876b97d20382412f62
Raylan Araujo <raylanra@gmail.com>
2025-06-10 18:15:24 -0300

    anotacoes sobre o que e Git

---
+++ 8_o_que_e_Git.py
@@ -0,0 +1,13 @@
+# Git é um sistema de versao distribuido
+# Gratuito e Open Source ( código aberto)
+#   
+#   em todo o site do git é possivel aprender sobre o sistema
+#   ler sempre a documentacao oficial da ferramenta
+#
+#   Comandos:
+#   git clone: para clonar o repositorio do servidor para uma pasta local
+#   git commit: grava as alteracoes no repositorio
+#   git pull: para atualizar o seu repositorio local a partir repositório que ta no servidor. Puxa as alteracoes do repositorio remoto para o repositório local e mesca com o arquivo que voce ta trabalhando
+#   git push: que ele emporra as alterações do repositorio local para o remoto.
+
+# pode haver algum conflitos
