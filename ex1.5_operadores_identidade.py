6121d031b5b68617fa5958b6b839e263354312e6
Raylan Araujo <raylanra@gmail.com>
2025-06-14 19:39:20 -0300

    anotacoes de operadores de identidade

---
+++ ex1.5_operadores_identidade.py
@@ -0,0 +1,5 @@
+saldo = 1000
+limite = 500
+
+print(saldo is limite) #quero saber se eles ocupam o mesmo lugar na memória
+print(saldo is not limite) #se eu querosaber se eles não ocupam o mesmo lugar na mamória
