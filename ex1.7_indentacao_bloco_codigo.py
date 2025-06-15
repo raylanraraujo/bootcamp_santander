444b2c84656508316224cfe7d564021460b5a99a
Raylan Araujo <raylanra@gmail.com>
2025-06-15 08:26:45 -0300

    anotacoes de indentacao e bloco de codigo

---
+++ ex1.7_indentacao_bloco_codigo.py
@@ -0,0 +1,14 @@
+def sacar(valor):
+    saldo = 500
+    
+    if saldo >= valor:
+        print("Valor sacado!")
+        print("Retire o seu dinheiro na boca do caixa!")
+
+    print("Obrigado por ser nosso cliente. Tenha um bom dia.\n")
+
+
+sacar(100)
+sacar(1000)
+
+# tudo que está desviado 4 espaços para direta, o interpretador entende que está dentro dobloco de código.
