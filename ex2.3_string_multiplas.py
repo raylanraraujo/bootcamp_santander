92012ade433f7a3e494a211d5b3090c2d8bfba58
Raylan Araujo <raylanra@gmail.com>
2025-06-16 06:59:06 -0300

    anotacooes de strings multiplas

---
+++ ex2.3_string_multiplas.py
@@ -0,0 +1,23 @@
+nome = "Guilherme"
+
+mensagem = f'''
+	Olá meu nome é {nome}, 
+Estou aprendendo Python.
+		Essa mensagem tem diferentes recuos.
+'''  
+
+print(mensagem)
+
+print(
+    """
+    =============== MENUE ===============
+
+    1 - Depositar
+    2 - Sacar
+    0 - Sair
+
+    =====================================
+
+        Obrigado por usar nosso sistema
+    """
+)
