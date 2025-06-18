652333dbcba56c9409c855c057ea08a7172120bc
Raylan Araujo <raylanra@gmail.com>
2025-06-18 18:41:59 -0300

    anotacoes listas e criacao

---
+++ ex3.0_listas_criacao_acesso.py
@@ -0,0 +1,13 @@
+frutas = ["laranja", "maca", "uva"]
+print(frutas)
+
+frutas = [] #posso declarar uma lista vazia
+print(frutas)
+
+letras = list("python") # cria uma lista onde cada letra seria um elemento
+print(letras)
+
+numeros = list(range(10)) # cria um elemento para cada valor do range - ou seja cria uma lista indo do 0 até o 9
+print(numeros)
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True] # lista onde fala todas as informações de um carro
+print(carro)
