9de37592f8e97a3c30cd5ca7525cacfc4d2c08d5
Raylan Araujo <raylanra@gmail.com>
2025-06-20 19:09:16 -0300

    atualizacao de anotacoes de conjunto

--- ex3.3_conjuntos_set.py
+++ ex3.3_conjuntos_set.py
@@ -1,11 +1,66 @@
+#criando conjuntos
 carros = ({"palio", "gol", "celta", "palio"}) # passado um objeto iterável pra o construtor set
 print(carros)
 
 numeros = {1, 2, 3, 4}
 print(numeros)
 
-set([1, 2, 3, 1, 3, 4]) 
+num = set([1, 2, 3, 1, 3, 4]) 
+print(num)
 
-set("abacaxi") 
+fruta = set("abacaxi") 
+print(fruta)
 
-set(("palio", "gol", "celta", "palio")) 
+populares = set(("palio", "gol", "celta", "palio")) 
+print(populares)
+
+
+# Acessando dados
+numeros = list(numeros)
+n1 = numeros[0]
+print(n1)
+
+# Iterar conjuntos
+numeros = set(numeros)
+
+for numero in numeros:
+    print(numero)
+
+# Funcao enumerate
+for indice, carro in enumerate(carros):
+    print(f"{indice}: {carro}")
+
+# Métodos da classe Set
+conjunto_a = {1, 2, 3}
+conjunto_b = {2, 3, 4}
+conjunto_c = {4, 1, 2, 5, 6, 3}
+conjunto_d = {1, 2, 3, 4, 5}
+conjunto_e = {6, 7, 8, 9}
+conjunto_f = {0, 1}
+
+# union
+print(conjunto_a.union(conjunto_b))
+
+# intersection
+print(conjunto_a.intersection(conjunto_b))
+
+#isdisjoint
+print(conjunto_d.isdisjoint(conjunto_e))
+print(conjunto_d.isdisjoint(conjunto_f))
+
+#add
+sorteio = {1, 23}
+
+sorteio.add(25)
+sorteio.add(42)
+sorteio.add(25)
+print(sorteio)
+
+#copy
+copia_sorteio = sorteio.copy()
+print(copia_sorteio)
+
+# clear
+sorteio.clear()
+print(sorteio)
+print(copia_sorteio)
