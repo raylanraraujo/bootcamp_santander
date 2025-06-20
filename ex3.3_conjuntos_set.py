9700d4a8a35a57f60c6e7b3cd41d1404bbfe3bfa
Raylan Araujo <raylanra@gmail.com>
2025-06-20 09:55:57 -0300

    iniciando as anotacoes de conjuntos

---
+++ ex3.3_conjuntos_set.py
@@ -0,0 +1,11 @@
+carros = ({"palio", "gol", "celta", "palio"}) # passado um objeto iterável pra o construtor set
+print(carros)
+
+numeros = {1, 2, 3, 4}
+print(numeros)
+
+set([1, 2, 3, 1, 3, 4]) 
+
+set("abacaxi") 
+
+set(("palio", "gol", "celta", "palio")) 
