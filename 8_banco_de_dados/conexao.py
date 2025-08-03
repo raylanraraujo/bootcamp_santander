d6eb83939437aa69c31dc4e4ff8988ac6206df7e
Raylan Araujo <raylanra@gmail.com>
2025-08-03 19:05:06 -0300

    manipulacao de banco de dados

--- 8_banco_de_dados/conexao.py
+++ 8_banco_de_dados/conexao.py
@@ -2,7 +2,29 @@ import sqlite3
 from pathlib import Path
 
 ROOT_PATH = Path(__file__).parent
-conexao = sqlite3.connect(ROOT_PATH / 'clientes.db')
 
-print(conexao)
+conexao = sqlite3.connect(ROOT_PATH / 'meu_banco.db')
+cursor = conexao.cursor()
 
+def criar_tabela (conexao, cursor):
+    cursor.execute(
+        "CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(150))"
+    )
+    conexao.commit()
+
+def inserir_registro(conexao, cursor, nome, email):
+    data= (nome, email)
+    cursor.execute("INSERT INTO clientes (nome, email) VALUES (?, ?)", data)
+    conexao.commit()       
+
+
+def atualizar_registro(conexao, cursor, nome, email, id):
+    data = (nome, email, id)
+    cursor.execute(
+        "UPDATE clientes SET nome = ? , email = ? WHERE id = ?", data
+    )
+    conexao.commit()
+
+
+atualizar_registro(conexao, cursor, "Guilherme Carvalho" , "guilherme@gmail.com", 1)
+atualizar_registro(conexao, cursor, "Giovanna Carvalho" , "giovanna@gmail.com",2)
